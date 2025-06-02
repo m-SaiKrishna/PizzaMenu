971389653c8f3732ce4ec8f5c3584426ff5debd0
m-SaiKrishna <saikrishna5112004@gmail.com>
2025-06-02 21:21:00 +0530

    component updadted

--- src/index.js
+++ src/index.js
@@ -62,7 +62,7 @@ function Header() {
   const style = {};
   return (
     <header className="header">
-      <h1 style={style}>Fast React Pizza Co.</h1>
+      <h1 style={style}>Instant React Pizza Co.</h1>
     </header>
   );
 }
